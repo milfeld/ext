2a8d3ec82ae67b5905f1a4db9a4642834c76dc04
Henry Jin <hjin@nas.nasa.gov>
2016-08-25 12:48:03 -0700

    added example for linear clause in loop constructs

---
+++ sources/Example_linear_in_loop.1.f90
@@ -0,0 +1,26 @@
+! @@name:	linear_in_loop.1f
+! @@type:	F-free
+! @@compilable:	yes
+! @@linkable:	yes
+! @@expect:	success
+program linear_loop
+   implicit none
+   integer, parameter :: N = 100
+   real :: a(N), b(N/2)
+   integer :: i, j
+
+   do i = 1, N
+      a(i) = i
+   end do
+
+   j = 0
+   !$omp parallel do linear(j:1)
+   do i = 1, N, 2
+      j = j + 1
+      b(j) = a(i) * 2.0
+   end do
+
+   print *, j, b(1), b(j)
+   ! print out: 50 2.0 198.0
+
+end program
